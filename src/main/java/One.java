def047a39d2ac8fe511086114f467f147796f971
user <majestik13@mail.ru>
2017-07-25 13:50:16 +0300

    third commit

--- src/main/java/One.java
+++ src/main/java/One.java
@@ -1,2 +1,11 @@
 public class One {
+
+
+    public static void main(String[] args) {
+        int one=5;
+        int second=45;
+        System.out.println( "Привет Гитхаб");
+        int b=one+second;
+        System.out.println(b);
+    }
 }
